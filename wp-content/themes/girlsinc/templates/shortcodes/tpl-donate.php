f079fef37890b87706f3c05e09ae0e24968c4640
Daron Adkins <daron.adkins@gmail.com>
2018-02-26 18:43:53 -0600

    more edits

--- wp-content/themes/girlsinc/templates/shortcodes/tpl-donate.php
+++ wp-content/themes/girlsinc/templates/shortcodes/tpl-donate.php
@@ -5,10 +5,9 @@
     <p>Give to ensure girls have the experiences and opportunities to thrive.</p>
     <div class="row">
         <div class="col-md-12">
-            <a href="/donate-now" id="donate-btn">Donate Now</a>
-            <?php
-            if(! is_page(694)) {
-                echo '<a href="/donate" class="offset-md-1 text-center">Learn More</a>';
+            <a href="https://crm.bloomerang.co/HostedDonation?ApiKey=pub_318e7c35-a482-11e7-afbe-024e165d44b3&WidgetId=202752" id="donate-btn">Click here to donate now</a>
+            <?php if (! is_page(694)) {
+                echo '<a href="/donate" style="margin-left: 30px;">Learn More</a>';
             }
             ?>
         </div>
