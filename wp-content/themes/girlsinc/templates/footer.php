cd06c5fd26bfc19e12b195852489a89dbc59ba6d
Daron Adkins <daron.adkins@gmail.com>
2018-02-28 14:17:28 -0600

    wip

--- wp-content/themes/girlsinc/templates/footer.php
+++ wp-content/themes/girlsinc/templates/footer.php
@@ -319,12 +319,12 @@
     </div>
     <div class="container-fluid container-dark">
         <div class="row footer-sidebar">
-            <div class="col-6 col-lg-3">
+            <div class="col-6">
                 <div class="row">
                     <?php dynamic_sidebar('sidebar-footer-init'); ?>
                 </div>
             </div>
-            <div class="col-6 col-lg-3">
+            <div class="col-6">
                 <div class="row">
                     <?php dynamic_sidebar('sidebar-footer'); ?>
                 </div>
